5c42c73b0f5cd300484f9cbb3ffa85091f0671c1
saurabhkabariya <163295904+saurabhkabariya@users.noreply.github.com>
2025-09-07 22:58:51 +0530

    removed local urls

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.backend_url || 'http://localhost:5000';
+const API_BASE_URL = process.env.backend_url;
+// const API_BASE_URL = process.env.backend_url || 'http://localhost:5000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
